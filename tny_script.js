834762ca26d5ab23306f8e1cb54ab6eb9806d3ff
Isaiah Romo <iromo237@west-mec.org>
2020-01-07 16:02:32 -0700

    day 1 of notes

---
+++ tny_script.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Isaiah Romo
+   Date:   1/6/20
+
+*/
+
+console.log("Hello World");
+console.log(4);
+console.log(2+1);
+debugger;
+console.log(3+"pies");
+console.log(4/0);
+
